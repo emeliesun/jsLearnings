d3c98e8901da9e22522d3d1e39c9319886949978
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-05-11 17:21:29 +0200

    Reverse String

---
+++ reverseString.js
@@ -0,0 +1,17 @@
+function reverseString(str) {
+  // Method 1
+  //  var strArr = str.split("");
+  //  var reverseStrArr = strArr.reverse();
+  //  var reverseStr = reverseStrArr.join("");
+  //  return reverseStr;
+  // Method 2
+  // return reverseStr = str.split("").reverse().join("");
+  //Method 3
+  //   var reverseStr = '';
+  //   for (let i = str.length - 1; i >= 0; i--) {
+  //     reverseStr += str[i];
+  //   }
+  //   return reverseStr;
+}
+
+console.log(reverseString('Good Day'));
