f0e421142c847785e82397cdc3568708c8fce15b
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-06-01 17:04:19 +0200

    calculate-assign kata

--- jsTrainKata.js
+++ jsTrainKata.js
@@ -18,3 +18,15 @@ function removeChar(str){
 removeChar('eloquent');
 removeChar('country');
 
+// Calculate number of paperwork needed, also if 'n' or 'm' is  less than 0, return 0.
+function paperwork(n, m) {
+  if (n <= 0 || m <= 0) {
+    return 0;
+  } else {
+    return n * m;
+  }
+}
+
+paperwork(5, 5);
+paperwork(5, -5);
+
