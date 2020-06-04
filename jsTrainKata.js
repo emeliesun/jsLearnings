928cc226789ce0692ff96b02a4e6b73c72b41090
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-06-04 12:30:20 +0200

    kata - clock in millisecs

--- jsTrainKata.js
+++ jsTrainKata.js
@@ -30,3 +30,28 @@ function paperwork(n, m) {
 paperwork(5, 5);
 paperwork(5, -5);
 
+// Return clock-time with hours, minutes and seconds converted to milliseconds.
+function past(h, m, s) {
+  do {
+    let oneH = h * 3600000;
+    let oneM = m * 60000;
+    let oneS = s * 1000;
+    return oneH + oneM + oneS;
+  } while (0 <= h <= 23 && 0 <= m <= 59 && 0 <= s <= 59);
+}
+
+                // another shorter solution:
+                function past(h, m, s) {
+                   let oneH = h * 3600000;
+                    let oneM = m * 60000;
+                    let oneS = s * 1000;
+                    return oneH + oneM + oneS;
+                }
+
+past(0, 1, 1);
+past(1, 1, 1);
+past(0, 0, 0);
+
+
+
+//
